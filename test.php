c7b6a3af669901a0cf4385cd98056e1901304e5a
tina0420 <32476325+tina0420@users.noreply.github.com>
2020-03-27 16:57:06 +0800

    destance-test ->get data successfully

--- test.php
+++ test.php
@@ -1,12 +1,30 @@
 <?php
-include "./data_base/get_address.php";
+// include "./data_base/get_address.php";
 
-// $get_address = new GetAddress();
-// $get_address->address = $_GET["address"]; // 讀取欄位地址
-// $get_address->get_address();
+// // $get_address = new GetAddress();
+// // $get_address->address = $_GET["address"]; // 讀取欄位地址
+// // $get_address->get_address();
 
-$api = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=[location]&destinations=[destinations]&key=[myKey]");
-$data = json_decode($api);
+// $api = file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=[location]&destinations=[destinations]&key=[myKey]");
+// $data = json_decode($api);
 
-echo ((int)$data->rows[0]->elements[0]->distance->value / 1000).' Km';
+// echo ((int)$data->rows[0]->elements[0]->distance->value / 1000).' Km';
+
+
+    include "./data_base/data.php";
+    include "./data_base/get_address.php";
+    $getAddress = new GetAddress();
+    $stmt = $getAddress->get_address_name();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($data);
+    
+    for($i=0; $i<count($data);$i++){
+        $driving_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=106台北市大安區仁愛路四段10號&destinations=".$data[$i]['address']."&key=AIzaSyB8kBxTI4-IP2ikLYNdKcLBTc-xOeDxeHE";
+        // $get_url = file_get_contents($driving_url);
+        $result = json_decode($driving_url);
+        $distance[$i] = $result->rows[0]->elements[0]->distance->text;
+        $time[$i] = $result->rows[0]->elements[0]->duration->text; 
+    }
+    echo min($distance);
+    echo min($time);
 ?>
